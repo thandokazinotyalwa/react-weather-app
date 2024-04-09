5b0723f7403a3316a3727186583efcd2e250b589
Thandokazi Notyalwa <thandonotyalwa@gmail.com>
2024-04-09 16:34:40 +0200

    removed unnecessary files and added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <div class="container">
+      <h1>Weather App</h1>
+      <footer>This project was coded by{" "}
+      <a href="https://www.delac.io/" target="blank">Thandokazi Notyalwa</a>{" "}
+      and is{" "}
+      <a href="https://www.delac.io/" target="blank">open-sourced on Github</a>
+      </footer>
+    </div>
+    </div>
+  );
 }
 
 
